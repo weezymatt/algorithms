4f357fab7d7da4d78bcea5ece229749c86e1bf12
85853890+weezymatt@users.noreply.github.com <mhernan0697@gmail.com>
2025-06-09 12:28:57 -0500

    feat: add python solution to 20. Valid Parentheses

---
+++ algorithms/python/ValidParentheses/ValidParentheses.py
@@ -0,0 +1,35 @@
+"""
+Source          : https://leetcode.com/problems/valid-parentheses/
+Name            : Matthew Hernandez
+Date            : 2025-06-09
+Data Structure  : Stack
+Algorithm       : Open-closed brackets, no initial closed, stack empty at the end
+Time            : O(n) — only loop through the len(s) once
+Space           : O(n) - worst case only appends all strings
+"""
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        brackets = {
+            "(" : ")",
+            "[" : "]",
+            "{" : "}",
+        }
+
+        if len(s) == 1:
+            return False
+        
+        for bracket in s:
+            if bracket in brackets:
+                stack.append(bracket)
+            elif stack and bracket == brackets[stack[-1]]:
+                stack.pop()
+            else:
+                return False
+
+        return len(stack) == 0
